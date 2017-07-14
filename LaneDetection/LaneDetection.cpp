a039ba51e8eb6fd88cd9dfa54241ccea04c5c625
jiahsuanlo <joshloqs@gmail.com>
2017-07-14 15:19:52 -0400

    -trying ground plane projection method

--- LaneDetection/LaneDetection.cpp
+++ LaneDetection/LaneDetection.cpp
@@ -3,6 +3,20 @@
 
 #include "stdafx.h"
 #include <opencv2\opencv.hpp>
+#include <expvideo\CameraUtility.h>
+
+struct changeConstrast
+{
+	void operator() (cv::Point3_<uint8_t> &pixel, const int *position) const
+	{
+		double alpha = 1.5;
+		int beta = -100;
+		pixel.x = cv::saturate_cast<uchar> (pixel.x*alpha + beta);
+		pixel.y = cv::saturate_cast<uchar> (pixel.y*alpha + beta);
+		pixel.z = cv::saturate_cast<uchar> (pixel.z*alpha + beta);
+	}
+};
+
 
 void findInnerEdges(const cv::Mat &img,
 	std::vector<cv::Point> &lpts,
@@ -70,8 +84,8 @@ void maskEdgeByPolygon(const cv::Mat &grayEdge, cv::Mat &dst)
 	int rows = grayEdge.rows;
 	pts[0][0] = (cv::Point(cols*0.05, rows*0.9));
 	pts[0][1] = (cv::Point(cols*0.95, rows*0.9));
-	pts[0][2] = (cv::Point(cols*0.45, 0.5*rows));
-	pts[0][3] = (cv::Point(cols*0.55, 0.5*rows));
+	pts[0][2] = (cv::Point(cols*0.45, 0.65*rows));
+	pts[0][3] = (cv::Point(cols*0.55, 0.65*rows));
 
 	int npts[] = { 4 };
 	const cv::Point* ppt[1] = { pts[0] };
@@ -86,19 +100,87 @@ void colorThresholding(const cv::Mat &src, cv::Mat &maskOut)
 	cv::cvtColor(src, edgeH, cv::COLOR_BGR2HSV);
 
 	// color thresholding
-	cv::Scalar lb(10, 105, 200); //30,145,255 yellow
-	cv::Scalar ub(40, 175, 255);
-	cv::Scalar lbw(10, 0, 200); //32,8,255 white
-	cv::Scalar ubw(40, 40, 255);
-	cv::Scalar lbg(160, 0, 140); //188,7,163 gray
-	cv::Scalar ubg(220, 40, 180);
-	cv::Mat edgeY, edgeW, edgeG;
-	cv::inRange(edgeH, lb, ub, edgeY);
+	cv::Scalar lby(14, 178, 127); //30,145,255 yellow
+	cv::Scalar uby(57, 255, 255);
+	cv::Scalar lbw(0, 0, 200); //32,8,255 white
+	cv::Scalar ubw(255, 255, 255);
+	//cv::Scalar lbg(160, 0, 140); //188,7,163 gray
+	//cv::Scalar ubg(220, 40, 180);
+	
+	cv::Mat edgeY, edgeW;//, edgeG;
+	cv::inRange(edgeH, lby, uby, edgeY);
 	cv::inRange(edgeH, lbw, ubw, edgeW);
-	cv::inRange(edgeH, lbg, ubg, edgeG);
+
+	//cv::inRange(edgeH, lbw, ubw, edgeW);
+	//cv::inRange(edgeH, lbg, ubg, edgeG);
 	cv::bitwise_or(edgeY, edgeW, maskOut);
-	cv::bitwise_or(maskOut, edgeG, maskOut);
+	//cv::bitwise_or(maskOut, edgeG, maskOut);
 }
+
+void constructPerspectiveMapping(cv::Mat &tm_vp, cv::Size imgSize)
+{
+	// build rotation matrix
+	cv::Mat rm_cv, rm_vc;
+	rotzyx(-90 * CV_PI / 180, 0, -90 * CV_PI / 180, rm_cv);
+	rm_vc = rm_cv.t();
+	// build 3x4 transformation matrix 
+	cv::Mat tm_vc= cv::Mat::zeros(3,4, CV_64FC1);
+	// fill in the rotation part
+	cv::Rect roi(0, 0, 3, 3);	
+	rm_vc.copyTo(tm_vc(roi));
+	
+	// build intrinsic matrix
+	double tx_cp = imgSize.width/2;
+	double ty_cp = imgSize.height/2;
+	double focal = 0.1;  // 10 cm
+	double pixelPerM = 10; // 10 pixel per meter
+	cv::Mat km = cv::Mat::eye(3, 3, CV_64FC1);
+	km.at<double>(0, 0) = pixelPerM*focal;
+	km.at<double>(1, 1) = pixelPerM*focal;
+	km.at<double>(0, 2) = tx_cp;
+	km.at<double>(1, 2) = ty_cp;
+	
+	// assemble output transformation matrix
+	tm_vp = km*tm_vc;
+}
+
+void getGroundImage(const cv::Mat &src, const cv::Mat &tm_vp,
+	cv::Mat &img)
+{
+	Plane3D gp;
+	gp.D = 0;
+	gp.setNormal(0, 0, 1);
+	VirtualCamera vc{ 0,0,10,0,-90 * CV_PI / 180,0 };
+	
+	cv::Mat xmap, ymap, zmap;
+	vc.hitPlane(gp, xmap, ymap, zmap);
+	// allocate output image size
+	img = cv::Mat::zeros(xmap.size(), CV_8UC3);
+
+	cv::Mat x_v = cv::Mat::zeros(3, 1, CV_64FC1);
+	cv::Mat x_p;
+	// fill in colors
+	for (int i = 0; i < src.rows; ++i)
+	{
+		for (int j = 0; j < src.cols; ++j)
+		{
+			
+			x_v.at<double>(0,0) = xmap.at<double>(i, j);
+			x_v.at<double>(1, 0) = ymap.at<double>(i, j);
+			x_v.at<double>(2, 0) = zmap.at<double>(i, j);
+			x_p= tm_vp*x_v;
+			x_p = x_p / x_p.at<double>(2, 0); // normalize
+			int u = int(x_p.at<double>(0, 0));
+			int v = int(x_p.at<double>(1, 0));
+			if (u>=0 && u<src.cols &&
+				v>=0 && v<src.rows)
+			{
+				img.at<cv::Vec3b>(i, j) = src.at<cv::Vec3b>(v, u);
+			}
+		}
+	}
+}
+
 
 void detectLane(const cv::Mat &src, cv::Mat &dst)
 {
@@ -115,21 +197,93 @@ void detectLane(const cv::Mat &src, cv::Mat &dst)
 	//cv::Mat gray;
 	//cv::cvtColor(src, gray, cv::COLOR_BGR2GRAY);
 
-	cv::Mat hsv;
-	cv::cvtColor(src, hsv, cv::COLOR_BGR2HSV);
+	// ehnace constrast
+	cv::Mat srcEh = src.clone();
+	srcEh.forEach<cv::Point3_<uint8_t>>(changeConstrast());
+	
+	cv:: Mat tm_vp;
+	constructPerspectiveMapping(tm_vp, srcEh.size());
 
+	cv::Mat srcG;
+	getGroundImage(src, tm_vp, srcG);
+
+	// gray
+	cv::Mat gray;
+	cv::cvtColor(srcEh, gray, cv::COLOR_BGR2GRAY);
+	
 	// canny edge
 	cv::Mat edge;
-	cv::Canny(src, edge, 100, 255);
+	cv::Canny(gray, edge, 100, 255, 5);
 
-	// mask
-	cv::Mat edgeB;
-	maskEdgeByPolygon(edge, edgeB);
-	
 	// dilate
-	cv::Mat elem = cv::getStructuringElement(cv::MORPH_ELLIPSE,
+	cv::Mat maskEdge;
+	cv::Mat elem = cv::getStructuringElement(cv::MORPH_RECT,
 		cv::Size(11, 11), cv::Point(5, 5));
-	cv::dilate(edgeB, edgeB, elem);
+	cv::dilate(edge, maskEdge, elem);
+	
+	// color the edge and convert color to hsv
+	cv::Mat hsv;
+	cv::bitwise_and(srcEh, srcEh, hsv, maskEdge);
+	cv::cvtColor(hsv, hsv, cv::COLOR_BGR2HSV);
+
+	// color thresholding
+	cv::Mat maskColor;
+	colorThresholding(hsv, maskColor);
+
+	// bound the image
+	cv::Mat maskBnd;
+	maskEdgeByPolygon(maskColor, maskBnd);
+	
+	// find inner edges
+	std::vector<cv::Point> lpts, rpts;
+	findInnerEdges(maskBnd, lpts, rpts);
+	//  debugging codes
+	cv::Mat ptImgL = cv::Mat::zeros(src.size(), CV_8UC1);
+	cv::Mat ptImgR = cv::Mat::zeros(src.size(), CV_8UC1);
+	for (auto pp : rpts)
+	{
+		cv::circle(ptImgR, pp, 3, cv::Scalar(255));
+		cv::circle(srcEh, pp, 3, cv::Scalar(255,0,0));
+	}
+	for (auto pp : lpts)
+	{
+		cv::circle(ptImgL, pp, 3, cv::Scalar(255));
+		cv::circle(srcEh, pp, 3, cv::Scalar(0,255,255));
+	}
+	/* debugging codes
+	for (auto p : lpts)
+	{
+	std::cout << "left points: " << p << "\n";
+	}
+	for (auto p : rpts)
+	{
+	std::cout << "right points: " << p << "\n";
+	}*/
+	
+	// hough lines
+	std::vector<cv::Vec4i> linesR, linesL;
+	std::vector<cv::Point> lptsHT, rptsHT;
+	cv::HoughLinesP(ptImgR, linesR, 10, CV_PI / 180, 50, 20, 10);
+	cv::HoughLinesP(ptImgL, linesL, 10, CV_PI / 180, 50, 20, 10);
+	for (size_t i = 0; i < linesR.size(); i++)
+	{
+		cv::Point pt1, pt2;
+		pt1.x = linesR[i][0];  pt1.y = linesR[i][1];
+		pt2.x = linesR[i][2];  pt2.y = linesR[i][3];
+		rptsHT.push_back(pt1);
+		rptsHT.push_back(pt2);
+		line(srcEh, pt1,pt2, cv::Scalar(0, 0, 255), 3, CV_AA);
+	}
+	for (size_t i = 0; i < linesL.size(); i++)
+	{
+		cv::Point pt1, pt2;
+		pt1.x = linesL[i][0];  pt1.y = linesL[i][1];
+		pt2.x = linesL[i][2];  pt2.y = linesL[i][3];
+		lptsHT.push_back(pt1);
+		lptsHT.push_back(pt2);
+		line(srcEh, pt1,pt2, cv::Scalar(0, 255, 255), 3, CV_AA);
+	}
+
 
 	// coloring edge
 	//cv::Mat edgeC;
@@ -138,52 +292,28 @@ void detectLane(const cv::Mat &src, cv::Mat &dst)
 	// color thresholding
 	//cv::Mat edgeMask;
 	//colorThresholding(edgeC, edgeMask);
-	
-	// find inner edges
-	std::vector<cv::Point> lpts, rpts;
-	findInnerEdges(edgeB, lpts, rpts);
-	/* debugging codes
-	for (auto p : lpts)
-	{
-		std::cout << "left points: " << p << "\n";
-	}
-	for (auto p : rpts)
-	{
-		std::cout << "right points: " << p << "\n";
-	}*/
-
-	dst = src.clone();
-	if (lpts.size() > 1 && rpts.size() > 1)
+		
+	/*if (lptsHT.size() > 1 && rptsHT.size() > 1)
 	{
 		cv::Vec4f lLine, rLine;
-		cv::fitLine(lpts, lLine, CV_DIST_L2, 0, 0.01, 0.01);
-		cv::fitLine(rpts, rLine, CV_DIST_L2, 0, 0.01, 0.01);
+		cv::fitLine(lptsHT, lLine, CV_DIST_L2, 0, 0.01, 0.01);
+		cv::fitLine(rptsHT, rLine, CV_DIST_L2, 0, 0.01, 0.01);
 
 		// get inner edges end points
 		cv::Point rp1, rp2, lp1, lp2;
-		getLineEndPoints(lLine, lpts, edgeB.rows - 1, lp1, lp2);
-		getLineEndPoints(rLine, rpts, edgeB.rows - 1, rp1, rp2);
+		getLineEndPoints(lLine, lptsHT, maskBnd.rows - 1, lp1, lp2);
+		getLineEndPoints(rLine, rptsHT, maskBnd.rows - 1, rp1, rp2);
 
 		// draw lines
-		cv::Mat edgeLine = cv::Mat::zeros(edgeB.size(), CV_8UC3);
+		cv::Mat edgeLine = cv::Mat::zeros(maskBnd.size(), CV_8UC3);
 		line(edgeLine, lp1, lp2, cv::Scalar(0, 0, 255), 10, 8);
 		line(edgeLine, rp1, rp2, cv::Scalar(0, 0, 255), 10, 8);
-
-		/*  debugging codes
-		for (auto pp : rpts)
-		{
-			cv::circle(edgeLine, pp, 3, cv::Scalar(0, 0, 255));
-		}
-		for (auto pp : lpts)
-		{
-			cv::circle(edgeLine, pp, 3, cv::Scalar(0, 230, 255));
-		}
-		*/
-
+		
 		// blend
-		cv::addWeighted(edgeLine, 0.5, src, 1.0, 0, dst);
-	}
-	dst = hsv.clone();
+		cv::addWeighted(edgeLine, 0.5, srcEh, 1.0, 0, dst);
+	}*/
+	
+	dst = srcG.clone();
 }
 
 void testImage(const std::string &fname)
